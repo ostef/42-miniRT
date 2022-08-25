eafe1f085932ff659b3be3608f572743f590fc3a
ostef <47391841+ostef@users.noreply.github.com>
2022-08-25 23:30:34 +0200

    Offset gradient by mouse position.

--- source/main.c
+++ source/main.c
@@ -10,20 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "ft_math.h"
-#include "win32_layer.h"
-#include <time.h>
+#include "miniRT.h"
 
-static void	set_pixel(t_window *win, int x, int y, t_u32 color)
+static inline void	set_pixel(t_window *win, int x, int y, t_u32 color)
 {
 	((t_u32 *)win->pixels)[y * win->frame_width + x] = color;
 }
 
-static t_u32	rgb(t_u8 r, t_u8 g, t_u8 b)
+static inline void	clear_frame(t_window *win, t_u32 color)
 {
-	return (r << 16) | (g << 8) | b;
+	for (int i = 0; i < win->frame_width * win->frame_height; i += 1)
+		((t_u32 *)win->pixels)[i] = color;
 }
+
+#define	rgb(r, g, b) (((r) << 16) | ((g) << 8) | (b))
 
 int	main(void)
 {
@@ -35,21 +35,22 @@ int	main(void)
 		ft_fprintln (STDERR, "Could not create window.");
 		return (1);
 	}
+	t_vec2f	offset = ft_vec2f(0,0);
 	while (win.opened)
 	{
 		poll_window_events (&win);
+		clear_frame (&win, 0);
+
+		offset = get_mouse_pos (&win);
 		for (int y = 0; y < win.frame_height; y += 1)
 		{
 			for (int x = 0; x < win.frame_width; x += 1)
 			{
-				float r = x / (float)win.frame_width;
-				float g = y / (float)win.frame_height;
-				float b = (cosf ((float)GetTickCount64 () * 0.001) + 1) * 0.5;
-				set_pixel (&win, x, y, rgb ((t_u8)(r * 255), (t_u8)(g * 255), (t_u8)(b * 255)));
+				set_pixel (&win, x, y, rgb ((t_u8)(x + offset.x * 255), (t_u8)(y - offset.y * 255), 0));
 			}
 		}
 		update_window (&win);
-		Sleep (16);
+		Sleep (10);
 	}
 	destroy_window (&win);
 	return (0);
