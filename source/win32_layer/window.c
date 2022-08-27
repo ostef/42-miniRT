2b1b427b108ff1d2519546500a1d6de2a628676e
ostef <47391841+ostef@users.noreply.github.com>
2022-08-27 19:32:57 +0200

    Renamed create_framebuffer to allocate_framebuffer, removed resize_framebuffer, use width and height parameters when creating a window.

--- source/win32_layer/window.c
+++ source/win32_layer/window.c
@@ -15,14 +15,16 @@
 static LRESULT	wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	t_window	*win;
-	HDC			hdc;
-	PAINTSTRUCT	paint;
+	RECT		rect;
 
 	win = (t_window *)GetWindowLongPtrA (hwnd, GWLP_USERDATA);
 	if (!win)
 		return (DefWindowProcA (hwnd, msg, wparam, lparam));
 	if (msg == WM_SIZE)
-		resize_framebuffer (win);
+	{
+		GetClientRect (win->hwnd, &rect);
+		allocate_framebuffer (win, rect.right - rect.left, rect.bottom - rect.top);
+	}
 	else if (msg == WM_CLOSE)
 		win->opened = FALSE;
 	return (DefWindowProcA (hwnd, msg, wparam, lparam));
@@ -51,13 +53,13 @@ t_bool	create_window(t_window *win, t_cstr title, int width, int height)
 	if (!register_wndclass())
 		return (FALSE);
 	win->hwnd = CreateWindowA("WindowClass", title, WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL,
+		CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL,
 		GetModuleHandleA (NULL), 0);
 	if (!win->hwnd)
 		return (FALSE);
 	SetWindowLongPtrA (win->hwnd, GWLP_USERDATA, (LONG_PTR)win);
 	GetClientRect (win->hwnd, &rect);
-	if (!create_framebuffer (win, rect.right - rect.left, rect.bottom - rect.top))
+	if (!allocate_framebuffer (win, rect.right - rect.left, rect.bottom - rect.top))
 		return (FALSE);
 	win->opened = TRUE;
 	return (TRUE);
@@ -87,10 +89,31 @@ void	poll_window_events(t_window *win)
 	}
 }
 
+t_bool	allocate_framebuffer(t_window *win, t_int width, t_int height)
+{
+	if (win->pixels)
+		ft_free (win->pixels, ft_heap());
+	win->pixels = ft_alloc (width * height * 4, ft_heap());
+	if (!win->pixels)
+		return (FALSE);
+	win->frame_width = width;
+	win->frame_height = height;
+	return (TRUE);
+}
+
 void	update_window(t_window *win)
 {
+	BITMAPINFO	bmi;
+
+	ft_memset (&bmi, 0, sizeof (bmi));
+	bmi.bmiHeader.biSize = sizeof (bmi.bmiHeader);
+	bmi.bmiHeader.biWidth = win->frame_width;
+	bmi.bmiHeader.biHeight = win->frame_height;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biCompression = BI_RGB;
 	StretchDIBits (GetDC (win->hwnd),
 		0, 0, win->frame_width, win->frame_height,
 		0, 0, win->frame_width, win->frame_height,
-		win->pixels, &win->bmp_info, DIB_RGB_COLORS, SRCCOPY);
+		win->pixels, &bmi, DIB_RGB_COLORS, SRCCOPY);
 }
