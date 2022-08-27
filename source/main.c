f7716c8404e646c083308e26ac539c4d358064e8
ostef <47391841+ostef@users.noreply.github.com>
2022-08-27 19:34:05 +0200

    Cast rays for each pixel, using camera position and direction. Test for each object in the scene (only spheres for now), and render the closest one.

--- source/main.c
+++ source/main.c
@@ -23,35 +23,112 @@ static inline void	clear_frame(t_window *win, t_u32 color)
 		((t_u32 *)win->pixels)[i] = color;
 }
 
+void forward_to_orthonormal_basis (t_vec3f forward, t_vec3f *out_right, t_vec3f *out_up)
+{
+	t_vec3f	right;
+	t_vec3f	up;
+
+	right = ft_vec3f_cross (ft_vec3f(0, 1, 0), forward);
+	if (ft_approx_zero (ft_vec3f_dot (right, right), 0.000001f))
+	{
+		up = ft_vec3f_cross (forward, ft_vec3f(1, 0, 0));
+		right = ft_vec3f_cross (up, forward);
+	}
+	else
+		up = ft_vec3f_cross (right, forward);
+	if (out_right)
+		*out_right = ft_vec3f_normalized (right);
+	if (out_up)
+		*out_up = ft_vec3f_normalized (up);
+}
+
+t_vec3f	get_camera_right(t_rt *rt)
+{
+	t_vec3f	right;
+
+	forward_to_orthonormal_basis (rt->camera_direction, &right, NULL);
+	return (right);
+}
+
+t_vec3f	get_camera_up(t_rt *rt)
+{
+	t_vec3f	up;
+
+	forward_to_orthonormal_basis (rt->camera_direction, NULL, &up);
+	return (up);
+}
+
 #define	rgb(r, g, b) (((r) << 16) | ((g) << 8) | (b))
 
 int	main(void)
 {
-	t_window	win;
+	t_rt	rt;
 
-	ft_memset (&win, 0, sizeof (win));
-	if (!create_window (&win, "miniRT", 640, 480))
+	ft_memset (&rt, 0, sizeof (rt));
+	rt.camera_direction = ft_vec3f(0, 0, 1);
+	if (!create_window (&rt.win, "miniRT", 640, 480))
 	{
 		ft_fprintln (STDERR, "Could not create window.");
 		return (1);
 	}
-	t_vec2f	offset = ft_vec2f(0,0);
-	while (win.opened)
+	t_object	*obj = add_sphere (&rt, ft_vec3f (0, 10, 200), 100.0);
+	obj->color = rgb(255, 0, 0);
+	
+	obj = add_sphere (&rt, ft_vec3f (10, -60, 150), 90.0);
+	obj->color = rgb (0, 255, 0);
+	
+	obj = add_sphere (&rt, ft_vec3f (-35, -14, 400), 200.0);
+	obj->color = rgb (0, 0, 255);
+	while (rt.win.opened)
 	{
-		poll_window_events (&win);
-		clear_frame (&win, 0);
+		t_u64	start = GetTickCount64 ();
 
-		offset = get_mouse_pos (&win);
-		for (int y = 0; y < win.frame_height; y += 1)
+		poll_window_events (&rt.win);
+		clear_frame (&rt.win, 0);
+
+		rt.camera_position.x += (is_key_down ('D') - is_key_down ('A'));
+		rt.camera_position.y += (is_key_down ('E') - is_key_down ('Q'));
+		rt.camera_position.z += (is_key_down ('W') - is_key_down ('S'));
+
+		t_vec3f	camera_right = get_camera_right (&rt);
+		t_vec3f	camera_up = get_camera_up (&rt);
+		t_vec3f	camera_plane = ft_vec3f_sub (
+			ft_vec3f_sub (rt.camera_position,
+				ft_vec3f_mulf (camera_right, rt.win.frame_width * 0.5f)
+			), ft_vec3f_mulf (camera_up, rt.win.frame_height * 0.5f));
+
+		for (int y = 0; y < rt.win.frame_height; y += 1)
 		{
-			for (int x = 0; x < win.frame_width; x += 1)
+			for (int x = 0; x < rt.win.frame_width; x += 1)
 			{
-				set_pixel (&win, x, y, rgb ((t_u8)(x + offset.x * 255), (t_u8)(y - offset.y * 255), 0));
+				t_ray	ray;
+				ray.origin = ft_vec3f_add (camera_plane, ft_vec3f_add (ft_vec3f_mulf (camera_right, x), ft_vec3f_mulf (camera_up, y)));
+				ray.dir = rt.camera_direction;
+				t_f32	closest_distance = ft_inf32 ();
+				t_s64	closest_object = -1;
+				for (int i = 0; i < rt.obj_count; i += 1)
+				{
+					if (rt.objs[i].shape != SPHERE)
+						continue ;
+					t_f32	distance;
+					if (ray_sphere_intersection (ray, rt.objs[i].sphere, &distance))
+					{
+						if (distance < closest_distance)
+						{
+							closest_distance = distance;
+							closest_object = i;
+						}
+					}
+				}
+				if (closest_object != -1)
+					set_pixel (&rt.win, x, y, rt.objs[closest_object].color);
 			}
 		}
-		update_window (&win);
+		update_window (&rt.win);
+
+		ft_println ("%u", GetTickCount64 () - start);
 		Sleep (10);
 	}
-	destroy_window (&win);
+	destroy_window (&rt.win);
 	return (0);
 }
