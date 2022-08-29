8e80f2b2fba3f68cd3bb6081cc179c9497878b64
ostef <47391841+ostef@users.noreply.github.com>
2022-08-29 16:09:37 +0200

    Perspective.

--- source/main.c
+++ source/main.c
@@ -23,41 +23,6 @@ static inline void	clear_frame(t_window *win, t_u32 color)
 		((t_u32 *)win->pixels)[i] = color;
 }
 
-void forward_to_orthonormal_basis (t_vec3f forward, t_vec3f *out_right, t_vec3f *out_up)
-{
-	t_vec3f	right;
-	t_vec3f	up;
-
-	right = ft_vec3f_cross (ft_vec3f(0, 1, 0), forward);
-	if (ft_approx_zero (ft_vec3f_dot (right, right), 0.000001f))
-	{
-		up = ft_vec3f_cross (forward, ft_vec3f(1, 0, 0));
-		right = ft_vec3f_cross (up, forward);
-	}
-	else
-		up = ft_vec3f_cross (right, forward);
-	if (out_right)
-		*out_right = ft_vec3f_normalized (right);
-	if (out_up)
-		*out_up = ft_vec3f_normalized (up);
-}
-
-t_vec3f	get_camera_right(t_rt *rt)
-{
-	t_vec3f	right;
-
-	forward_to_orthonormal_basis (rt->camera_direction, &right, NULL);
-	return (right);
-}
-
-t_vec3f	get_camera_up(t_rt *rt)
-{
-	t_vec3f	up;
-
-	forward_to_orthonormal_basis (rt->camera_direction, NULL, &up);
-	return (up);
-}
-
 #define	rgb(r, g, b) (((r) << 16) | ((g) << 8) | (b))
 
 int	main(void)
@@ -71,14 +36,12 @@ int	main(void)
 		ft_fprintln (STDERR, "Could not create window.");
 		return (1);
 	}
-	t_object	*obj = add_sphere (&rt, ft_vec3f (0, 10, 200), 100.0);
-	obj->color = rgb(255, 0, 0);
+
+	add_sphere (&rt, ft_vec3f (0, 1, 20), 10.0)->color = rgb(255, 0, 0);
+	add_sphere (&rt, ft_vec3f (1, -6, 15), 9.0)->color = rgb (0, 255, 0);
+	add_sphere (&rt, ft_vec3f (-3.5, -1.4, 40), 20.0)->color = rgb (0, 0, 255);
 	
-	obj = add_sphere (&rt, ft_vec3f (10, -60, 150), 90.0);
-	obj->color = rgb (0, 255, 0);
-	
-	obj = add_sphere (&rt, ft_vec3f (-35, -14, 400), 200.0);
-	obj->color = rgb (0, 0, 255);
+	float scale = tanf (60.0f * 0.5f * PI / 180.0f);
 	while (rt.win.opened)
 	{
 		t_u64	start = GetTickCount64 ();
@@ -86,24 +49,25 @@ int	main(void)
 		poll_window_events (&rt.win);
 		clear_frame (&rt.win, 0);
 
-		rt.camera_position.x += (is_key_down ('D') - is_key_down ('A'));
-		rt.camera_position.y += (is_key_down ('E') - is_key_down ('Q'));
-		rt.camera_position.z += (is_key_down ('W') - is_key_down ('S'));
+		rt.camera_position.x += (is_key_down ('D') - is_key_down ('A')) * 1;
+		rt.camera_position.y += (is_key_down ('E') - is_key_down ('Q')) * 1;
+		rt.camera_position.z += (is_key_down ('W') - is_key_down ('S')) * 1;
+	
+		float image_aspect_ratio = rt.win.frame_width / (float)rt.win.frame_height;
+		float inv_width = 1 / (float)rt.win.frame_width;
+		float inv_height = 1 / (float)rt.win.frame_height;
+		t_mat4f	view_matrix = ft_mat4f_look_at(rt.camera_position, rt.camera_direction);
 
-		t_vec3f	camera_right = get_camera_right (&rt);
-		t_vec3f	camera_up = get_camera_up (&rt);
-		t_vec3f	camera_plane = ft_vec3f_sub (
-			ft_vec3f_sub (rt.camera_position,
-				ft_vec3f_mulf (camera_right, rt.win.frame_width * 0.5f)
-			), ft_vec3f_mulf (camera_up, rt.win.frame_height * 0.5f));
-
+		t_ray	ray;
+		ray.origin = ft_mat4f_transform_point (view_matrix, rt.camera_position);
 		for (int y = 0; y < rt.win.frame_height; y += 1)
 		{
+			float yy = (1 - 2 * (y + 0.5) * inv_height) * scale;
 			for (int x = 0; x < rt.win.frame_width; x += 1)
 			{
-				t_ray	ray;
-				ray.origin = ft_vec3f_add (camera_plane, ft_vec3f_add (ft_vec3f_mulf (camera_right, x), ft_vec3f_mulf (camera_up, y)));
-				ray.dir = rt.camera_direction;
+				float xx = (2 * (x + 0.5) * inv_width - 1) * image_aspect_ratio * scale;
+
+				ray.dir = ft_mat4f_transform_vector (view_matrix, ft_vec3f_normalized (ft_vec3f (xx, yy, 1)));
 				t_f32	closest_distance = ft_inf32 ();
 				t_s64	closest_object = -1;
 				for (int i = 0; i < rt.obj_count; i += 1)
